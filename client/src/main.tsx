175db2a6c5b30e3a525f89e536c1a7ef7e5d8358
Ethan Cooke <ethandcooke@gmail.com>
2024-03-28 16:02:12 -0300

    further work on home page

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -37,14 +37,18 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
 function Layout() {
   return (
     <>
-      <div className="flex flex-col w-full h-screen">
-        <div className="bg-blue-800 text-white p-4 w-full flex gap-4">
+      <div className="flex flex-col w-full min-h-screen">
+        <div className="bg-[#34332f] text-white p-4 w-full flex gap-4">
           <Link to="/">Home</Link>
           <Link to="/test">Test</Link>
           <Link to="/expenses">Expenses</Link>
         </div>
-        <Outlet />
-        <div className="bg-blue-800 text-white p-2 w-full text-center">&copy; CSCI 3461 Database Systems Group 5</div>
+        <div className="flex bg-[#3e3d39] flex-grow justify-center">
+            <div className="flex w-full p-4 max-w-7xl">
+              <Outlet />
+            </div>
+        </div>
+        <div className="bg-[#34332f] text-white p-2 w-full text-center">&copy; CSCI 3461 Database Systems Group 5</div>
       </div>
     </>
   )
@@ -53,22 +57,28 @@ function Layout() {
 function Home() {
   return (
     <>
-      <main className="prose max-w-none py-2 gap-2 flex flex-1 flex-col">
-        <div className="flex w-full justify-between gap-2">
-          <input className="bg-slate-600 flex-1 text-center text-gray-100 rounded" type="button" value="Cars"></input>
-          <input className="bg-slate-600 flex-1 text-center text-gray-100 rounded" type="button" value="Cars"></input>
-          <input className="bg-slate-600 flex-1 text-center text-gray-100 rounded" type="button" value="Cars"></input>
+      <div className="w-full gap-4 flex flex-col">
+        <div className="mx-auto my-24 gap-4 p-4 text-center text-[#a4b16c] rounded-lg flex flex-col">
+          <span className="font-bold text-4xl">MUC Database</span>
+          <span className="font-semibold">Group 5: Ethan Cooke, Justin Kaiser, Travis Burke</span>
         </div>
-        <div className="flex w-full justify-between gap-2">
-          <input className="bg-slate-600 flex-1 text-center text-gray-100 rounded" type="button" value="Cars"></input>
-          <input className="bg-slate-600 flex-1 text-center text-gray-100 rounded" type="button" value="Cars"></input>
-          <input className="bg-slate-600 flex-1 text-center text-gray-100 rounded" type="button" value="Cars"></input>
+        <div className="w-full flex flex-col gap-4">
+          <div className="flex w-full justify-between gap-4">
+            <input className="table-buttons" type="button" value="Cars"></input>
+            <input className="table-buttons" type="button" value="Cars"></input>
+            <input className="table-buttons" type="button" value="Cars"></input>
+          </div>
+          <div className="flex w-full justify-between gap-4">
+            <input className="table-buttons" type="button" value="Cars"></input>
+            <input className="table-buttons" type="button" value="Cars"></input>
+            <input className="table-buttons" type="button" value="Cars"></input>
+          </div>
         </div>
-      </main>
+      </div>
     </>
   )
 }
 
 function Test() {
-  return <div className="flex flex-1">Hello, Test!</div>
+  return <div className="flex flex-grow">Hello, Test!</div>
 }
