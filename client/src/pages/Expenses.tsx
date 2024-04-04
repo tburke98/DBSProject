267887624ebfd1458ac31a6dbbd21d7eb5375e5b
Ethan Cooke <ethandcooke@gmail.com>
2024-04-04 00:09:08 -0300

    added functionality of expense page

--- client/src/pages/Expenses.tsx
+++ client/src/pages/Expenses.tsx
@@ -1,16 +1,36 @@
 import useAxios from 'axios-hooks'
 import {AgGridReact} from '@ag-grid-community/react'
 import {useState} from 'react'
+import {useForm, SubmitHandler} from 'react-hook-form'
+import cx from 'classnames'
+
+import type {ColDef, GridOptions} from '@ag-grid-community/core'
+
+interface Expense {
+  year: number
+  total_expense: number
+}
 
 export default function Expenses() {
-  const [{data, loading, error}] = useAxios('expenses')
 
-  const [colDefs, setColDefs] = useState([
-    {field: 'year'},
-    {field: 'month'},
-    {field: 'electricity'},
-    {field: 'water'},
-    {field: 'heat'}
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: {errors}
+  } = useForm()
+  const [formData, setFormData] = useState('{"startYear":"0", "endYear":"0"}');
+
+  const current = JSON.parse(formData)
+
+  const [{data, loading, error}] = useAxios<Expense[], any, any>({
+    url: `expenses/${current.startYear}/${current.endYear}`,
+    method: 'GET'
+  })
+
+  const [columnDefs, _] = useState<ColDef[]>([
+    {field: 'year', headerName: 'Year', sort: "asc"},
+    {field: 'total_expense', headerName: 'Total Expenses'},
   ])
 
   if (loading) {
@@ -20,9 +40,26 @@ export default function Expenses() {
     return <pre>{JSON.stringify(error, null, 2)}</pre>
   }
 
+  const gridOptions: GridOptions = {
+    autoSizeStrategy: {
+      type: 'fitCellContents'
+    }
+  }
+
   return (
-    <div className="ag-theme-alpine" style={{height: 500}}>
-      <AgGridReact rowData={data} columnDefs={colDefs} />
-    </div>
+    <>
+      <div className="flex flex-col w-full gap-4">
+        <form className="flex gap-4 p-4" onSubmit={handleSubmit((formdata) => setFormData(JSON.stringify(formdata)))}>
+          <label htmlFor="start">Start Year:</label>
+          <input id="start" {...register("startYear", { required: true })} placeholder='startYear' />
+          <label htmlFor="end">End Year:</label>
+          <input id="end" {...register("endYear", { required: true })} placeholder='endYear' />
+          <input type="submit" />
+        </form>
+        <div className="ag-theme-alpine-dark w-full" style={{height: 500}}>
+          <AgGridReact rowData={data} columnDefs={columnDefs} gridOptions={gridOptions} />
+        </div>
+      </div>
+    </>
   )
 }
