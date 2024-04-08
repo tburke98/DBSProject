01eb731348917be2a7005b31c62e16079a0bfa15
Ethan Cooke <ethandcooke@gmail.com>
2024-04-07 23:10:53 -0300

    updated deploy script

--- scripts/deploy.ps1
+++ scripts/deploy.ps1
@@ -3,6 +3,6 @@ $user = "u72"
 $api_ver = 0.1.0
 ssh-keyscan $server >> ~/.ssh/known_hosts
 ssh -i ./dbcourse.pem $user@$server "rm -rf ~/public_html/*"
-scp -i ./dbcourse.pem -r "../client/dist/{.*,*}" $user@$($server):~/public_html
+scp -i ./dbcourse.pem -r "../client/dist/(.*|*)" $user@$($server):~/public_html
 scp -i ./dbcourse.pem -r "../api/dist/api-$api_ver.tar.gz" $user@$($server):~/project_api
-ssh -i ./dbcourse.pem $user@$server "cd ~/project_api && rm -rf api && tar -xzvf api-`$($api_ver).tar.gz && cd api && pip install -r requirements.txt"
+ssh -i ./dbcourse.pem $user@$server "cd ~/project_api && rm -rf api && tar -xzvf api-$($api_ver).tar.gz && cd api && pip install -r requirements.txt"
